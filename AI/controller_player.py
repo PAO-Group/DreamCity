f985326cab856e3e2d1101ec3f5a68a41e2d74a4
liudanpao <200724@qq.com>
2025-06-09 12:24:26 +0800

    feat(AI): 添加工作命令和玩家状态控制
    新增了`CmdDoSomework`命令类，用于表示工作命令，包含工作时间属性。
    在`CtrlPlayer`类中添加了`state`属性，初始值为'idle'，用于表示玩家的当前状态。
    在`do_command`方法中增加了对`CmdDoSomework`命令的处理逻辑，如果玩家状态为'working'则返回False，否则将状态设置为'working'，模拟工作一段时间后将状态重置为'idle'。
    在`do_command`方法中对已有的`CmdWebBroadCast`和`CmdWebShowMessageFromEntity`命令处理后增加了返回True的逻辑，以明确表示命令处理成功。

--- AI/controller_player.py
+++ AI/controller_player.py
@@ -14,23 +14,39 @@ class CmdWebShowMessageFromEntity(Command):
 	message: str
 	pass
 
+class CmdDoSomework(Command):
+	'''
+	工作命令
+	'''
+	time: int # 工作时间
+	pass
+
 class CtrlPlayer(CtrlOutter):
 	'''
 	玩家控制器
 	'''
 	commands = [CmdWebBroadCast]
 	web_api: IWebApi
+	state: str = 'idle' # 玩家状态
 	async def run(self):
 		# 从web端接收输入命令并反馈给parent控制器执行
 		# parent控制器执行结果向web端反馈
 		command = await self.web_api.get_request(self.owner)
 		if command is not None:
-			self.owner.push_command(command)
+			self.owner.push_command(command) # 向实体推送异步命令
 
 	async def do_command(self, command: Command):
 		# 向web端发送命令
 		if isinstance(command, CmdWebBroadCast) \
 			or isinstance(command, CmdWebShowMessageFromEntity):
-			await self.web_api.post_response(self.owner, command)
+			await self.web_api.post_response(self.owner, command) # 同步等待函数的执行结果
+			return True
+		elif isinstance(command, CmdDoSomework):
+			if self.state == 'working':
+				return False # 玩家正在工作中，不能再次工作
+			
+			self.state = 'working'
+			await asyncio.sleep(command.time)			# 模拟工作的执行
+			self.state = 'idle'
 			return True
 		return False
